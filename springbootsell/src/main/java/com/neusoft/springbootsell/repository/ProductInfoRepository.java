64226466933acf8e8f6beccc17be4b428619d6d2
xiehuanfa <1119361599@qq.com>
2021-05-20 17:00:38 +0800

    Create ProductInfoRepository.java

---
+++ springbootsell/src/main/java/com/neusoft/springbootsell/repository/ProductInfoRepository.java
@@ -0,0 +1,15 @@
+package com.neusoft.springbootsell.repository;
+
+import com.neusoft.springbootsell.dataobject.ProductCategory;
+import com.neusoft.springbootsell.dataobject.ProductInfo;
+import com.neusoft.springbootsell.enums.ProductStatusEnum;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductInfoRepository extends JpaRepository<ProductInfo,String> {
+   List<ProductInfo> findByProductStatus(Integer ProductStatus);
+
+
+
+}
