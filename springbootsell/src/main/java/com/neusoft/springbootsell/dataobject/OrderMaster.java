3c5aa9696cd740861052447b0a35700a1141b040
xiehuanfa <1119361599@qq.com>
2021-05-20 16:45:03 +0800

    Update OrderMaster.java

--- springbootsell/src/main/java/com/neusoft/springbootsell/dataobject/OrderMaster.java
+++ springbootsell/src/main/java/com/neusoft/springbootsell/dataobject/OrderMaster.java
@@ -1,5 +1,7 @@
 package com.neusoft.springbootsell.dataobject;
 
+import com.neusoft.springbootsell.enums.OrderStatusEnum;
+import com.neusoft.springbootsell.enums.PayStatusEnum;
 import lombok.Data;
 
 import javax.persistence.Entity;
@@ -16,22 +18,26 @@ public class OrderMaster {
 /** 订单id */
     private String orderId;
     /** 买家名字 */
-    private String userName;
+    private String buyerName;
     /** 买家手机号 */
     private String buyerPhone;
     /** 买家地址  */
     private String buyerAddress;
     /** 买家微信 openId*/
-    private String buyerOpenId;
+    private String buyerOpenid;
     /** 订单总金额 */
     private BigDecimal orderAmount;
     /** 订单状态 默认为0 新下单*/
-    private Integer orderStatus=0;
+    private Integer orderStatus = OrderStatusEnum.NEW.getCode();
+
     /** 支付状态 默认为0 未支付*/
-    private Integer payStatus=0;
+    private  Integer payStatus = PayStatusEnum.WAIT.getCode();
+
     /** 创建时间*/
     private Date createTime;
+
     /** 更新时间*/
     private Date updateTime;
+
 
 }
